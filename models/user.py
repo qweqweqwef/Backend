d75102a1f733dba8a814bd66da8a9e6489165921
Wen Liang <piggylegend520@gmail.com>
2019-05-07 10:54:36 +0800

    WIP

--- models/user.py
+++ models/user.py
@@ -6,14 +6,14 @@ class User(BaseModel):
     username = pw.CharField(unique=True)
     password = pw.CharField(index=True)
     email = pw.CharField(unique=True,index=True)
-    avatar_url = pw.TextField(null=True)
+    # avatar_url = pw.TextField(null=True)
 
     def as_dict(self):
         json_obj = {
             'id' : self.id,
             'username':self.username,
             'email' : self.email,
-            'avatar_url' : self.avatar_url,
+            # 'avatar_url' : self.avatar_url,
         }
 
         return json_obj
